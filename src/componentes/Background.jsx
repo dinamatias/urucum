f01daef01bb5746225c3aa361b7fe5bd1d8c7e25
Dina Matias <dina.hsmatias@gmail.com>
2025-06-04 15:44:18 -0300

    style: Cor de fundo e grafismos adicionados.

---
+++ src/componentes/Background.jsx
@@ -0,0 +1,13 @@
+// src/components/Background.jsx
+import React from "react";
+
+import "../estilos/Background.css";
+
+export default function Background() {
+  return (
+    <div className="background">
+      <div className="top-half"></div>
+      <div className="bottom-half"></div>
+    </div>
+  );
+}
